9fc1a3aad403ee6bb4ee9b842d88208b951ec815
Swiss Cheese Knife <swisscheeseknife@gmail.com>
2011-09-20 17:34:01 +0100

    Initial commit.

---
+++ run_mod.bat
@@ -0,0 +1,1 @@
+@"c:\program files (x86)\steam\steamapps\compleatenoob\source sdk base 2007\hl2.exe" -dev -game "c:\program files (x86)\steam\steamapps\SourceMods\protocol7" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
